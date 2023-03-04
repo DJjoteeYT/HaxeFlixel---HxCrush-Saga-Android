eaab4d930718f777b36056703c3f1f44171a5b84
DJjoteeYT <95763645+DJjoteeYT@users.noreply.github.com>
2023-03-04 11:25:48 +0100

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -12,6 +12,25 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, GameNoTweens));
+		
+		
+
+	
+
+		final ratioX:Float = stageWidth / gameWidth;
+
+		final ratioY:Float = stageHeight / gameHeight;
+
+		final zoom:Float = Math.min(ratioX, ratioY);
+
+		gameWidth = Math.ceil(stageWidth / zoom);
+
+		gameHeight = Math.ceil(stageHeight / zoom);
+
+		addChild(new FlxGame(gameWidth, gameHeight, GameNoTweens, zoom));
+
+	}
+
+}
 	}
 }
